72b05d505ccf977d8759c2cc2f102dc11d7abb15
lydianiu9328@gmail.com <lydianiu9328@gmail.com>
2024-11-19 19:59:30 -0800

    advice about nutrition from lydia

--- Recipe.h
+++ Recipe.h
@@ -14,6 +14,10 @@ class Recipe {
         string cuisine;
         vector<string> steps;
         double rating;
+        /*below is my concern (lydia): (based on the perspective from the datasest)
+        1. ingredients is a super long string, maybe vector is not a perfect fit
+        2. I need a subclass for nutrition. 
+        */
     public:
         void editRecipe();
         void viewRecipe();
