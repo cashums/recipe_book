9fbbf0fd1dba8a025d7c70f4000843d838af41f7
lydianiu9328@gmail.com <lydianiu9328@gmail.com>
2024-11-20 02:15:24 -0800

    new finding

--- Ingredient.h
+++ Ingredient.h
@@ -1,2 +1,11 @@
 // I believe we can parse the data and then extract the ingredients from the data
 // and push back into the ingredients vector
+
+//updates...the test_recipe have the ingredients name separately, but not int he big recipe.csv
+/*
+recipe: "8 small Granny Smith apples, or as needed, ½ cup unsalted butter, 3 tablespoons all-purpose flour, ½ cup white sugar, ½ cup packed brown sugar, ¼ cup water, 1 (9 inch) double-crust pie pastry, thawed"
+
+test_recipe: {'quantity': '½', 'unit': 'teaspoon', 'name': 'dried oregano'}, {'quantity': '4', 'unit': 'medium', 'name': 'skinless, boneless chicken breast halves - pounded 1/4 inch thick'}, {'quantity': '4', 'unit': 'tablespoons', 'name': 'butter'}, {'quantity': '4', 'unit': 'tablespoons', 'name': 'olive oil'}, {'quantity': '1', 'unit': 'cup', 'name': 'sliced mushrooms'}, {'quantity': '½', 'unit': 'cup', 'name': 'Marsala wine'}, {'quantity': '¼', 'unit': 'cup', 'name': 'cooking sherry'}]"
+
+
+*/
