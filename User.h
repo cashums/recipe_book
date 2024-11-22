fddb361b4a3e6e411a33735dfa7108aa1225888b
lydianiu9328@gmail.com <lydianiu9328@gmail.com>
2024-11-22 12:25:44 -0800

    delete nutrition

--- User.h
+++ User.h
@@ -6,24 +6,23 @@
 #include <vector>
 
 class User{
-    string userName = "";
-    int userID = 0;
     void signUp(const string&, const int&);
     bool logIn(const string&, const int&);
     void logOut();
 
     void displayHist();
-    void addSearchingHist(int &, Book& book);
-
     void displayFav();
-    void addFavorRecipe(int &, Book& book);
 
     ~User();
 
 private:
+    string userName = "";
+    int userID = 0;
     string password;
     bool authentication(const string&, const int&)const;
     vector<Recipe> searchingHistory_vec;
     vector<Recipe> favoriteRecipe_vec;
-    string password;
+
+    void addSearchingHist(Recipe*);Lydi
+    void addFavorRecipe(Recipe*);
 };
