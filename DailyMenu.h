1f534b530d0c9564932630a9a527dcc5e914713e
lydianiu9328@gmail.com <lydianiu9328@gmail.com>
2024-11-22 21:30:03 -0800

    update daily menu.h

--- DailyMenu.h
+++ DailyMenu.h
@@ -1,10 +1,11 @@
 #pragma once
 
 #include "User.h"
+#include "Recipe.h"
 
 #include <vector>
 
 class DailyMenu : public User{
-    vector<Recipe> dailyMenu(3);
+    vector<Recipe> dailyMenu[3];
     vecotr<Recipe> generateMenu(vector<Recipe>&, vector<Recipe>&);
 }
