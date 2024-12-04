f36f4a7a0e0ef2bbf55a1bf6bd872006d82df54e
lydianiu9328@gmail.com <lydianiu9328@gmail.com>
2024-12-03 20:56:16 -0800

    update sign in log in func in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include "Book.h"
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 #include <vector>
 
 using namespace std;
@@ -9,63 +12,97 @@ using namespace std;
 
 void filteringPage(){}
 
-
-User* signUp(vector<User*> &userVec){
+bool userExist(const string & name, const string &password = ""){
+    ifstream file("userInfo.csv");
+    string line, existingUserName, existingPassword;
+    while (getline(file, line)) {
+        stringstream ss(line);
+        getline(ss, existingUserName, ',');
+        getline(ss, existingPassword, ',');
+        
+        if (existingUserName == name) {
+            if (existingPassword == password) return true;
+        }
+    }
+    return false;
+}
+User* logIn(){
     string userName, password;
-    int user_input;
+    cout << "What's your user name: ";
+    cin >> userName;
+    cout << "What's your password: ";
+    cin >> password;
+
+    if (userExist(userName, password)) {
+        cout << "Login successful! Welcome back, " << userName << "." << endl;
+        User* new_user = new User(userName, password);
+        // read the searching hist/ favorite recipes from the csv file into the vector 
+        // -----------------------------------
+        return new_user;
+    } else {
+        int user_input;
+        cout << "Invalid username or password. Input 1 for sign up and 2 for log in: ";
+        cin >> user_input;
+
+        while (cin.fail() || (user_input != 1 && user_input != 2)) {
+            cout << "Your input is invalid. Please input again: ";
+            cin >> user_input;
+        }
+
+        if (user_input == 1) {
+            signUp();
+        } else if (user_input == 2) {
+            logIn();
+        }
+    }
+}
+
+
+User* signUp(){
+    string userName, password;
     cout << "What's your user name (please input letters/special characters/numbers without space): ";
     cin >> userName;
     cout << "What's your password (please input letters/special characters/numbers without space): ";
     cin >> password;
-    for (int i = 0; i < userVec.size(); i++){
-        // Dereference the pointer to access the User object
-        User* userPointer = userVec[i];       // Get the pointer
-        User& user = *userPointer;           // Dereference the pointer to get the object
 
-        if (user.getUserName() == userName){
-            cout << endl << "This user name is already being used. Input 1 for sign up and 2 for log in: ";
+    // Check if the user already exists
+    if (userExist(userName)) {
+        int user_input;
+        cout << "This user name is already being used. Input 1 for sign up and 2 for log in: ";
+        cin >> user_input;
+
+        while (cin.fail() || (user_input != 1 && user_input != 2)) {
+            cout << "Your input is invalid. Please input again: ";
             cin >> user_input;
-            while (cin.fail() || user_input != 1 || user_input != 2){
-                cout << endl << "Your input is invalid. Please input again: ";
-                cin >> user_input;
-            }
-            if (user_input == 1){ signUp(userVec); }
-            else if(user_input == 2){ logIn(userVec); }
+        }
+
+        if (user_input == 1) {
+            signUp();
+        } else if (user_input == 2) {
+            logIn();
         }
     }
+
+    // Append the new user to the CSV file
+    ofstream file("userInfo.csv", ios::app);
+    file << userName << "," << password << "\n";
+    file.close();
+
+    cout << "Sign-up successful! You can now log in." << endl;
+
     User* new_user = new User(userName, password);
-    userVec.push_back(new_user);
     return new_user;
 }
 
-User* logIn(vector<User*> &userVec){
-    string userName, password;
-    int user_input;
-    cout << "What's your user name: " << endl;
-    cin >> userName;
-    cout << "What's your password: " << endl;
-    cin >> password;
-    for (int i = 0; i < userVec.size(); i++){
-        User* userPointer = userVec[i];
-        User& user = *userPointer;
-        if (user.getUserName() == userName){
-            if (user.getUserPassword() == password){
-                user.logIn(userName, password);
-                return userPointer;
-            }else{
-                cout << "Incorrect password. Please try again..." << endl;
-                logIn(userVec);
-            }
-        }else{
-            cout << endl << "This user name is not registered yet. Input 1 for sign up and 2 for log in: ";
-            cin >> user_input;
-            while (cin.fail() || user_input != 1 || user_input != 2){
-                cout << endl << "Your input is invalid. Please input again: ";
-                cin >> user_input;
-            }
-            if (user_input == 1){ signUp(userVec); }
-            else if(user_input == 2){ logIn(userVec); }
-        }
+User* authentication(int &userInput_int){
+    while (cin.fail() || userInput_int != 1 || userInput_int != 2){
+        cout << "Your input is invalid. Please input again: ";
+        cin >> userInput_int;
+    }
+    if (userInput_int == 1){ 
+        return signUp();
+    }else if(userInput_int == 2){ 
+        return logIn(); 
     }
 }
 
@@ -78,8 +115,7 @@ void mainMenu(User &user){
 
 int main(){
     Book book;
-    int user_id = 0;
-    vector <User*> userVec;
+
     User* user_ptr = nullptr;
     int userInput_int = 0;
     string userName, password;
@@ -89,12 +125,7 @@ int main(){
          << "(enter 1 for sign up, 2 for log in): ";
     cin >> userInput_int;
 
-    while (cin.fail() || userInput_int != 1 || userInput_int != 2){
-        cout << "Your input is invalid. Please input again: ";
-        cin >> userInput_int;
-    }
-    if (userInput_int == 1){ user_ptr = signUp(userVec);
-    }else if(userInput_int == 2){ user_ptr = logIn(userVec); }
+    user_ptr = authentication(userInput_int);
 
     User& user = *user_ptr;
 
