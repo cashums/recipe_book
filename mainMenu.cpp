966ead62233b8d655334f4d20bf1a056bfe540e3
MuhammadS04 <muhammadsabeel4@gmail.com>
2024-12-06 11:41:09 -0800

    Fixed Memory Leaks

--- mainMenu.cpp
+++ mainMenu.cpp
@@ -2,11 +2,13 @@
 
 using namespace std;
 
-MainMenu::MainMenu() {
+MainMenu::MainMenu()
+{
     isRunning = true; // Menu starts in a running state
 }
 
-void MainMenu::start() {
+void MainMenu::start()
+{
     handleAuthentication();
 
     cout << "\n------------Welcome to Recipe Book------------" << endl;
@@ -14,14 +16,16 @@ void MainMenu::start() {
     Output::waitingScreen();
     Output::clearScreen();
 
-    while (isRunning) {
+    while (isRunning)
+    {
         displayMenu();
 
         cout << endl;
         int choice;
         cin >> choice;
 
-        if (cin.fail()) {
+        if (cin.fail())
+        {
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
             cout << "Invalid input. Please enter a number.\n";
@@ -32,7 +36,8 @@ void MainMenu::start() {
     }
 }
 
-void MainMenu::displayMenu() {
+void MainMenu::displayMenu()
+{
     Output::clearScreen();
     cout << "\nChoose what action you would like to take: "
          << "\n\t1. View all current recipes. "
@@ -43,8 +48,10 @@ void MainMenu::displayMenu() {
          << "\n\t6. Exit Cookbook.\n";
 }
 
-void MainMenu::handleChoice(int choice) {
-    switch (choice) {
+void MainMenu::handleChoice(int choice)
+{
+    switch (choice)
+    {
     case 1:
         Output::clearScreen();
         book.viewAllRecipes();
@@ -68,12 +75,14 @@ void MainMenu::handleChoice(int choice) {
         int recipeIndex;
         cin >> recipeIndex;
 
-        if (cin.fail() || recipeIndex < 0 || recipeIndex >= book.getBook().size()) {
+        if (cin.fail() || recipeIndex < 0 || recipeIndex >= book.getBook().size())
+        {
             cin.clear();                                         // Clear the error flag on `cin`
             cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignore invalid input
             cout << "Invalid input. Please enter a valid recipe number.\n";
-        } 
-        else {
+        }
+        else
+        {
             // Add the selected recipe to the favorite vector
             currentUser.addFavorRecipe(recipeIndex);
             cout << "Recipe added to favorites!" << endl;
@@ -89,9 +98,12 @@ void MainMenu::handleChoice(int choice) {
         fav_recipe_vec = currentUser.getFavRecVec();
 
         // Check if the favorite recipe list is empty
-        if (fav_recipe_vec.empty()) {
+        if (fav_recipe_vec.empty())
+        {
             cout << "You don't have any favorite recipes yet. Add some to your favorites!" << endl;
-        } else {
+        }
+        else
+        {
             // Display the favorite recipes using the user's printRecipe method
             cout << "\nHere are your favorite recipes: " << endl;
             currentUser.printRecipe(fav_recipe_vec, book.getBook());
@@ -108,30 +120,27 @@ void MainMenu::handleChoice(int choice) {
     }
 }
 
-void MainMenu::handleAuthentication() {
-    User user;
-    int userInput_int = 0;
+void MainMenu::handleAuthentication()
+{
+    User *userPtr = nullptr;
 
-    cout << "\n\t\t\t\tWelcome to the Recipe Book!" << endl
-         << "Would you like to sign up for a new account or log in?" << endl
-         << "(enter 1 for sign up, 2 for log in):\n";
+    cout << "Would you like to sign up for a new account or log in? (1 for sign up, 2 for log in): ";
+    int choice;
+    cin >> choice;
 
-    while (true) {
-        cin >> userInput_int;
-
-        // Check for invalid input or out-of-range values
-        if (cin.fail() || (userInput_int != 1 && userInput_int != 2)) {
-            cout << "Invalid input. Please enter 1 for sign up or 2 for log in: ";
-            cin.clear();                                         // Clear the error flag on `cin`
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignore invalid input
-        } else {
-            break; // Valid input, exit the loop
-        }
+    if (choice == 1)
+    {
+        userPtr = currentUser.signUp();
+    }
+    else if (choice == 2)
+    {
+        userPtr = currentUser.logIn();
     }
 
-    if (userInput_int == 1) {
-        currentUser.signUp();
-    } else if (userInput_int == 2) {
-        currentUser.logIn();
+    if (userPtr != nullptr)
+    {
+        // Use the user object
+        currentUser = *userPtr; // Copy the user object to currentUser
+        delete userPtr;         // Clean up dynamically allocated memory
     }
 }
